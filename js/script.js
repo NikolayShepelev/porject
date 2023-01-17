7b4c858075969ae8b7eac827d7f7c644d9accec3
ShNik <58742752+NikolayShepelev@users.noreply.github.com>
2023-01-17 19:47:11 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
+$(document).ready(function () {
+
+    // ТАРИФЫ
+
+    $('.plans__card-starting').mouseover(function () {
+        $(this).addClass('scaled');
+        $('.plans__card-business').addClass('business-scale');
+    });
+    $('.plans__card-starting').mouseout(function () {
+        $(this).removeClass('scaled');
+        $('.plans__card-business').removeClass('business-scale');
+    });
+    $('.plans__card-vip').mouseover(function () {
+        $(this).addClass('scaled');
+        $('.plans__card-business').addClass('business-scale');
+    });
+    $('.plans__card-vip').mouseout(function () {
+        $(this).removeClass('scaled');
+        $('.plans__card-business').removeClass('business-scale');
+    });
+
+
+    // ОТЗЫВЫ
+
+    $('.reviews__slider').on(`init reInit`, function (event, slick) {
+        $('.slick-slide-num-current').text('0' + 1 + ' ');
+    })
+    $('.reviews__slider').on(`afterChange`, function (event, slick, currentSlide, nextSlide) {
+        $('.slick-slide-num-current').text('0' + (currentSlide + 1) + ' ');
+    })
+    $('.reviews__slider').slick({
+        arrows: true,
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        adaptiveHeight: true,
+        easing: 'ease',
+        appendArrows: '.slick__arrows',
+        prevArrow: '<span class="slick-prev"><</span>',
+        nextArrow: '<span class="slick-next">></span>',
+    });
 
